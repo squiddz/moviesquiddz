a3425812ef860853a44aa0d01e8cfe3ae07715eb
squiddz <squiddz@gmail.com>
2009-08-24 21:46:37 +0100

    new demo.html

---
+++ files/highslide.js
@@ -0,0 +1,10 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.showCredits = false;
+hs.creditsPosition = 'bottom left';
+hs.outlineType = 'custom';
+hs.dimmingOpacity = 0.3;
+hs.allowMultipleInstances = false;
+hs.enableKeyListener = false;
+hs.numberOfImagesToPreload = 0;
